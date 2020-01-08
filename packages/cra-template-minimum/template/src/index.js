ca943087c6ab0aef8c8e1ec242579cd85fb4d1e2
Williamcsa04 <williamcaetano14@gmail.com>
2020-01-08 01:47:56 -0300

    :art: Removed text from starter index.js

--- packages/cra-template-minimum/template/src/index.js
+++ packages/cra-template-minimum/template/src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>React Minimum Template</h1>, document.getElementById('root'));
+ReactDOM.render(<div></div>, document.getElementById('root'));
